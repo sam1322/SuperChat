c05aabb497e8c802855b472cd96fd5726e12c97b
sam1322 <sriram.pant5@gmail.com>
2022-02-17 19:38:47 +0530

    Almost completed the chat

---
+++ superchat/src/ChatMessage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ChatMessage( { message:{text , uid ,photoURL} , auth}) {
+   const messageClass = uid === auth.currentUser.uid ?'sent' : 'recieved' ;  
+  return (
+      <div className={`message ${messageClass}`}>
+          <img src={photoURL}/>
+          <p>{text}</p>
+      </div>
+  )
+}
+
+export default ChatMessage
